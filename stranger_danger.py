935c2fd0e9b06f5261e0997a8935dd79ffd3eac5
AlbinSverreson <albin.sverreson@gmail.com>
2021-11-13 12:22:46 +0100

    lmao yeet

---
+++ stranger_danger.py
@@ -0,0 +1,17 @@
+import json
+import math
+
+with open("souls.json") as file:
+  data = json.load(file)
+  min = 2147483647
+  for i, n in enumerate(data):
+    for n_2 in data[i+1:]:
+      if abs(n + n_2) < min:
+        min = abs(n + n_2)
+        lmao = n
+        yeet = n_2
+        
+  print(min)
+  print(lmao)
+  print(yeet)
+
