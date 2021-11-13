63436854de232720e485128f0eee22833360c0a3
Nils Ceberg <nils.ceberg@gmail.com>
2021-11-13 16:56:57 +0100

    8: hack solution

---
+++ 8/hack.py
@@ -0,0 +1,8 @@
+x=-976316669
+y=976316682
+a=-956755151
+b=956755146
+
+terms = [x,y]*5 + [a,b]*13
+print(terms)
+print(sum(terms))
--- 8/stranger_danger.py
+++ 8/stranger_danger.py
@@ -7,9 +7,13 @@ with open("souls.json") as file:
   for i, n in enumerate(data):
     for n_2 in data[i+1:]:
       if abs(n + n_2) < min:
+        # modified to get data for hack.py
         min = abs(n + n_2)
-        lmao = n
+        lmao = n 
         yeet = n_2
+        print(lmao)
+        print(yeet)
+        print(n + n_2)
         
   print(min)
   print(lmao)
